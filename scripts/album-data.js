12ccb4361cd11d650f18eff801ce441fb481d46b
Billy Rose <theebillyrose@gmail.com>
2019-12-28 20:03:09 -0700

    first commit of updates

---
+++ scripts/album-data.js
@@ -0,0 +1,13 @@
+const album = {
+  title: 'Beef Briefs',
+  artist: 'Bishop Beef',
+  releaseInfo: '2019 Bovine Sacrament',
+  albumArtUrl: 'assets/images/album_covers/13.png',
+  songs: [
+    { title: 'Silent Night', duration: '2:41', soundFileUrl: 'assets/music/silent-night.mp3' },
+    { title: 'Donna House', duration: '3:32', soundFileUrl: 'assets/music/donna-house.mp3' },
+    { title: 'Even Tho', duration: '2:16', soundFileUrl: 'assets/music/even-tho.mp3' },
+    { title: 'Les Temps', duration: '2:13', soundFileUrl: 'assets/music/les-temps.mp3' },
+    { title: 'Sunnin', duration: '2:22', soundFileUrl: 'assets/music/sunnin.mp3' }
+  ]
+};
---
+++ scripts/helper.js
@@ -0,0 +1,13 @@
+class Helper {
+
+  playPauseAndUpdate(song) {
+    player.playPause(song);
+    var totalTime = player.getDuration();
+    $('#time-control .total-time').text(player.prettyTime(totalTime));
+  };
+
+
+
+}
+
+const helper = new Helper();
